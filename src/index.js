c719991422abfa96090637709dc384279b2450cc
olga.aheichyk <olga.aheichyk@gmail.com>
2021-02-17 09:19:25 +0300

    feat: create functions min, max & avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (Array.isArray(array) && array.length > 0) {
+        let min = array[0];
+        for (let i=1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+
+        return min;
+    }
+
+    return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (Array.isArray(array) && array.length > 0) {
+        let max = array[0];
+        for (let i=1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+        return max;
+    }
+    
+    return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (Array.isArray(array) && array.length > 0) {
+        let avg = (array.reduce((sum, current) => sum + current, 0)) / array.length;
+        return avg;
+    }
+
+    return 0;
 }
